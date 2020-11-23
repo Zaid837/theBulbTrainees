af347b3b0c35b5752410e75f379a5f82b25b0658
zaid837 <ayomide.zaid@gmail.com>
2020-11-23 15:49:49 +0100

    Created private repo

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let trainee = document.getElementById('trainee');
+console.log(trainee);
+
+function openModal () {
+    document.querySelector('.bg-modal').classList.toggle('open');
+}
+function closeModal () {
+    document.querySelector('.bg-modal').classList.toggle('open');
+    
+}
+
