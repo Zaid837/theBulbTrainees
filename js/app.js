320f4e3e8582bd667842ad1a55d3b539435b2562
bimbooladele@outlook.com <abimbola.oladele@thebulb.africa>
2020-11-25 11:03:05 +0100

    fix: added spinner and got user data

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 let trainee = document.getElementById("trainee");
-
+const trainees = document.querySelector(".trainees");
+const spinner = document.querySelector(".spinner");
 function openModal() {
   document.querySelector(".bg-modal").classList.toggle("open");
 }
 function closeModal() {
   document.querySelector(".bg-modal").classList.toggle("open");
 }
-const fellows = [];
-
 const getUsers = () => {
   fetch("https://5fbe19de5923c90016e6a815.mockapi.io/users")
     .then((res) => res.json())
     .then((user) => {
-      fellows.push(user)
-      fellows.forEach((fellow) =>fellows.push(fellow))
-      console.log(fellows)
+      displayUsers(user);
     });
 };
 getUsers();
+const displayUsers = fellows=>{
+    trainees.removeChild(spinner)
+      fellows.forEach(fellow=>{
+        trainees.innerHTML +=`
+        <div class="trainee" onclick="openModal()">
+          <img src=${fellow.avatar} alt="avi" class="profile">
+          <p>${fellow.name}</p>
+         </div>
+        `
+      })
+}
+
+{/* <div class="trainee" onclick="openModal()">
+  <img src="./images/default-placeholder-image.png" alt="avi" class="profile">
+  <p>Bakare Oluwatosin</p>
+</div> */}
