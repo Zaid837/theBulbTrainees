f0971a9c36d4830d60511b7097ec98a17e0ddc1a
James Morgan O <MorganJay@users.noreply.github.com>
2020-11-25 20:35:12 +0100

    added close modal function, fixing profile display

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
-let trainee = document.getElementById("trainee");
-const trainees = document.querySelector(".trainees");
-const spinner = document.querySelectorAll(".spinner");
-const modalCon = document.querySelector(".modal-con");
+  let trainee = document.getElementById("trainee");
+  const trainees = document.querySelector(".trainees");
+  const spinner = document.querySelectorAll(".spinner");
+  const modalCon = document.querySelector(".modal-con");
+  const modalBackground = document.querySelector(".bg-modal");
+
 function openModal(i) {
   document.querySelector(".bg-modal").classList.toggle("open");
-  getUser(i)
+  getUser(i);
 }
 function closeModal() {
   document.querySelector(".bg-modal").classList.toggle("open");
-  modalCon.innerHTML = '<div class="spinner">'
+  modalCon.innerHTML = '<div class="spinner">';
 }
-const getUser =(i)=>{
+
+modalBackground.addEventListener(("click"), e => {
+  if (e.path[0].classList.contains("open")) {
+    closeModal();
+  }
+});
+const getUser = (i) => {
   fetch(`https://5fbe19de5923c90016e6a815.mockapi.io/users/${i}`)
     .then((res) => res.json())
     .then((user) => {
-      displayUser(user)
+      displayUser(user);
     });
-}
+};
 const getUsers = () => {
   fetch("https://5fbe19de5923c90016e6a815.mockapi.io/users")
     .then((res) => res.json())
@@ -25,20 +33,19 @@ const getUsers = () => {
     });
 };
 
-const displayUsers = fellows=>{
-    spinner[0].style.display = 'none'
-      fellows.forEach((fellow,i)=>{
-        trainees.innerHTML +=`
-        <div class="trainee" onclick="openModal(${i+1})">
+const displayUsers = (fellows) => {
+  spinner[0].style.display = "none";
+  fellows.forEach((fellow, i) => {
+    trainees.innerHTML += `
+        <div class="trainee" onclick="openModal(${i + 1})">
           <img src=${fellow.avatar} alt="avi" class="profile">
           <p>${fellow.name}</p>
          </div>
-        `
-      })
-}
+        `;
+  });
+};
 
-
-const displayUser =(user)=>{
+const displayUser = (user) => {
   modalCon.innerHTML = `<div class="row mt-5">
   <div class="col-sm-4">
       <div class="modal-img">
@@ -46,7 +53,7 @@ const displayUser =(user)=>{
       </div>
   </div>
   <div class="col-sm-8">
-      <button class="close" onclick="closeModal()">&times;</button>
+      <button class="close close-btn" onclick="closeModal()">&times;</button>
   
       <div class="modal-info pt-5">
           <h3 class="name">${user.name}</h3>
@@ -57,8 +64,6 @@ const displayUser =(user)=>{
           <p class="socials">social links</p>
       </div>
   
-  </div>`
-}
+  </div>`;
+};
 getUsers();
-
-
